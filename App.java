d3b59375b1fe477ab74ae2f3117240742208f198
Lekgole <54614094+lekgole@users.noreply.github.com>
2019-10-27 21:32:03 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package org.twitter4j.twitter4j_stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
